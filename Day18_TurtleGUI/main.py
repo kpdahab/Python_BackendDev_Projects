8094126d2cc2d503c74309db8be7504b3ded3d2f
kpdahab <dahabkp@gmail.com>
2022-10-10 14:48:22 -0400

    Day 15 to 18

---
+++ Day18_TurtleGUI/main.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+import random
+
+tp = Turtle()
+tp.color("black")
+
+# Random Walk with a list of colours
+# colours = ["deep pink", "medium spring green", "firebrick", "medium blue"]
+# directions = [0, 90, 180, 270]
+# tp.pensize(5)
+#
+# for x in range(200):
+#     tp.color(random.choice(colours))
+#     tp.forward(10)
+#     tp.setheading(random.choice(directions))
+
+# # Random Walk with RGB function
+# def random_rgb_colour():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     random_rgb_colour = (r, g, b)
+#     return random_rgb_colour
+#
+# tp.pensize(5)
+# directions = [0, 90, 180, 270]
+#
+# for x in range(200):
+#     tp.color(random_rgb_colour())
+#     tp.forward(0)
+#     tp.setheading(random.choice(directions))
+
+#172. Spirograph
+
+
